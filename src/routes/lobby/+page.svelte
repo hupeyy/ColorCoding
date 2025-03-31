930402aa1771408e27f1de226006ff279bb6131e
EdwinASalcedo <92866752+EdwinASalcedo@users.noreply.github.com>
2025-03-31 17:33:28 -0400

    Added styling to lobby page

--- src/routes/lobby/+page.svelte
+++ src/routes/lobby/+page.svelte
@@ -2,6 +2,7 @@
     import { onMount } from 'svelte';
     import { lobbies, getLobbies, createGuest, createLobby, guestUser, joinLobby, getGuestLobbies } from '$lib/firebase';
     import * as Table from '$lib/components/ui/table';
+    import { Button } from '$lib/components/ui/button';
 
     let displayName = '';
     let lobbyName = '';
@@ -45,7 +46,7 @@
 
         <Table.Caption>Lobbies Available</Table.Caption>
 
-        <Table.Header>
+        <Table.Header class="text-2xl">
             <Table.Row>
                 {#each headers as header}
                     <Table.Head>{header}</Table.Head>
@@ -63,7 +64,7 @@
                     <Table.Cell>{lobby.DSA}</Table.Cell>
 
                     <Table.Cell>
-                        <button on:click={() => handleLobbyJoin(lobby.id)}>Join</button>
+                        <Button on:click={() => handleLobbyJoin(lobby.id)}>Join</Button>
                     </Table.Cell>
                 </Table.Row>
             {/each}
@@ -72,16 +73,20 @@
     </Table.Root>
 
     <!-- sample guest lobby creation -->
-     <input type="text" bind:value={displayName} placeholder="Enter Display Name" />
-     <button on:click={handleGuestCreation}>Set Name</button>
+     <div class="flex items-center justify-center p-6">
 
-     <input type="text" bind:value={lobbyName} placeholder="Enter Lobby Name" />
-     <input type="number" bind:value={maxPlayers} min="1" max="100" />
-     <label>
+     <input type="text" bind:value={displayName} placeholder="Enter Display Name" class="px-2" />
+     <Button on:click={handleGuestCreation} class="px-4">Set Name</Button>
+
+     <input type="text" bind:value={lobbyName} placeholder="Enter Lobby Name" class="px-2"/>
+     <p class="pl-2">Max Players: </p>
+     <input type="number" bind:value={maxPlayers} min="1" max="100" class="pr-2"/>
+     <label class="px-4">
         <input type="checkbox" bind:checked={DSA} />
         DSA Enabled
      </label>
-     <button on:click={handleLobbyCreation}>Create Lobby</button>
+     <Button on:click={handleLobbyCreation} class="px-4">Create Lobby</Button>
 
-     <h2>Hi, {currName}!</h2>
+    </div>
+    <h2 class="flex items-center justify-center p-6">Hi, {currName}!</h2>
 {/if}
